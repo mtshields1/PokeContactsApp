8f598c6f71c11233fa8cc3c19d0f494c96bd441f
Mike Shields <mtshields1@gmail.com>
2016-12-22 16:41:27 -0500

    Added Avatar functionality
    Added code so that the WebView gifs, for the contact avatars, can be displayed for each contact.

--- app/src/main/java/com/example/owner/pokecontacts/PhoneBookAdapter.java
+++ app/src/main/java/com/example/owner/pokecontacts/PhoneBookAdapter.java
@@ -3,6 +3,7 @@ package com.example.owner.pokecontacts;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -53,8 +54,48 @@ public class PhoneBookAdapter extends BaseAdapter
             convertView = inflater.inflate(R.layout.phonebook_row, null);
         }
 
-        ImageView ivAvatar = (ImageView)convertView.findViewById(R.id.imgAvatar);
-        ivAvatar.setImageBitmap(entry.getmAvatar());
+        //-----< this is where the gif avatars will be set >-----
+        if (entry.getmName().toString().equals("Mekem")){
+            WebView wv = (WebView)convertView.findViewById(R.id.webView);
+            wv.loadUrl("file:///android_asset/shiny_shinx.gif");
+            wv.getSettings().setLoadWithOverviewMode(true);
+            wv.getSettings().setUseWideViewPort(true);
+            wv.setClickable(false);
+            wv.setLongClickable(false);
+            wv.setFocusable(false);
+            wv.setFocusableInTouchMode(false);
+        }
+        else if (entry.getmName().toString().equals("Yo Khurl")){
+            WebView wv = (WebView)convertView.findViewById(R.id.webView);
+            wv.loadUrl("file:///android_asset/hitmonchan.gif");
+            wv.getSettings().setLoadWithOverviewMode(true);
+            wv.getSettings().setUseWideViewPort(true);
+            wv.setClickable(false);
+            wv.setLongClickable(false);
+            wv.setFocusable(false);
+            wv.setFocusableInTouchMode(false);
+        }
+        else if (entry.getmName().toString().equals("Dan Dan")){
+            WebView wv = (WebView)convertView.findViewById(R.id.webView);
+            wv.loadUrl("file:///android_asset/salamence.gif");
+            wv.getSettings().setLoadWithOverviewMode(true);
+            wv.getSettings().setUseWideViewPort(true);
+            wv.setClickable(false);
+            wv.setLongClickable(false);
+            wv.setFocusable(false);
+            wv.setFocusableInTouchMode(false);
+        }
+        else
+        {
+            WebView wv = (WebView)convertView.findViewById(R.id.webView);
+            wv.loadUrl("file:///android_asset/pidgey.gif");
+            wv.getSettings().setLoadWithOverviewMode(true);
+            wv.getSettings().setUseWideViewPort(true);
+            wv.setClickable(false);
+            wv.setLongClickable(false);
+            wv.setFocusable(false);
+            wv.setFocusableInTouchMode(false);
+        }
 
         TextView tvName = (TextView)convertView.findViewById(R.id.tvName);
         tvName.setText(entry.getmName());
