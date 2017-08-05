4486f26bf284554dd0d9a352ac7f432d06529401
mtshields1 <mtshields1@gmail.com>
2017-08-05 15:23:27 -0400

    Added a contact call button

--- app/src/main/java/com/example/owner/pokecontacts/ContactInfo.java
+++ app/src/main/java/com/example/owner/pokecontacts/ContactInfo.java
@@ -1,5 +1,7 @@
 package com.example.owner.pokecontacts;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
-public class ContactInfo extends AppCompatActivity {
-
+public class ContactInfo extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -19,31 +21,31 @@ public class ContactInfo extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         doStuff();
     }
 
     public void doStuff()
     {
-        Android_Contact curr_contact = null;
         Bundle contactBundle = this.getIntent().getExtras();
         if (contactBundle != null)
         {
-            curr_contact = (Android_Contact)contactBundle.getSerializable("current_contact");
+            final Android_Contact curr_contact = (Android_Contact)contactBundle.getSerializable("current_contact");
 
             TextView contactName = (TextView) findViewById(R.id.conName);
             contactName.setText(curr_contact.getmName());
 
             TextView contactPhone = (TextView)findViewById(R.id.conPhone);
             contactPhone.setText(curr_contact.getmPhone());
+
+            FloatingActionButton callButton = (FloatingActionButton) findViewById(R.id.callbutton);
+            callButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", curr_contact.getmPhone(), null));
+                    startActivity(intent);
+                }
+            });
         }
     }
 
