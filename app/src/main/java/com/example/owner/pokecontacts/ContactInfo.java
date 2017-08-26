b08e81853984be554b974a02e32c2e9d2fb988ab
mtshields1 <mtshields1@gmail.com>
2017-08-26 16:34:41 -0400

    Added contact's name below their avatar

--- app/src/main/java/com/example/owner/pokecontacts/ContactInfo.java
+++ app/src/main/java/com/example/owner/pokecontacts/ContactInfo.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
@@ -39,11 +40,13 @@ public class ContactInfo extends AppCompatActivity
         if (contactBundle != null) {
             final Android_Contact curr_contact = (Android_Contact) contactBundle.getSerializable("current_contact");
 
+            //-----< Get the contact's pokedex number for their avatar and create the name of the gif file >-----
             StringBuilder avatarString = new StringBuilder();
             int pokedexNumber = curr_contact.getPokemonAvatarNumber();
             String pokemonName = pokeList.get(pokedexNumber);
             avatarString.append(pokemonName + ".gif");
 
+            //-----< Create the Drawee view that will show the contact's avatar, a gif file >-----
             SimpleDraweeView draweeView = (SimpleDraweeView)findViewById(R.id.gifView);
             Uri uri = Uri.parse("asset:///3D/" + avatarString.toString());
             DraweeController controller =
@@ -56,11 +59,9 @@ public class ContactInfo extends AppCompatActivity
 
             avatarString.setLength(0);     //clear the stringbuilder
 
-            TextView contactName = (TextView) findViewById(R.id.conName);
-            contactName.setText(curr_contact.getmName());
-
-            TextView contactPhone = (TextView) findViewById(R.id.conPhone);
-            contactPhone.setText(curr_contact.getmPhone());
+            //-----< Display the contact's name below their avatar >-----
+            TextView contactText = (TextView)findViewById(R.id.contactView);
+            contactText.setText(curr_contact.getmName());
 
             //-----< Set up the click listener for the call button >-----
             FloatingActionButton callButton = (FloatingActionButton) findViewById(R.id.callbutton);
